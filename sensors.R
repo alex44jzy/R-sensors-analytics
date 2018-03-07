5eb9ca94ef10a24bbf4a7e9e166bb991cc6c9584
Janet27729 <janet27729@gmail.com>
2018-03-07 15:26:30 +0800

    janet test

--- sensors.R
+++ sensors.R
@@ -10,6 +10,7 @@ library(magrittr)
 library(lubridate)
 library(rlist)
 # test
+# janet
 data_march = read.csv("/Users/alexjzy/Desktop/R-projects/sensor-data-prep/Assignment_Data.csv")
 data_april = read.csv("/Users/alexjzy/Desktop/R-projects/sensor-data-prep/Assignment_Data2.csv")
 names(data_march)
